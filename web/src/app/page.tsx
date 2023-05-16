79c6a48f683562794ffaabaae67c7cd4586cbab0
Wanderson Castro <wandersonce1@hotmail.com>
2023-05-16 14:28:39 -0700

    Initial Tailwind tests

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-   <h1>Hello World</h1>
+    <div className="h-screen bg-zinc-950">
+      <h1 className="text-zinc-50 text-4xl font-bold p-6">Your Time Capsule</h1>
+    </div>
   )
 }
