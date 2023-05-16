69c959f5336072e21db5c9264d81fc5a96c22cbc
Wanderson Castro <wandersonce1@hotmail.com>
2023-05-16 12:59:21 -0700

    Adjusting ESLINT

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
-app.get('/hello', () =>{
+app.get('/hello', () => {
   return 'Hello World'
-} );
-
-app.listen({
-  port:3333,
-}).then(() => {
-  console.log('Listening on port 3333...🤞');
 })
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Listening on port 3333...🤞')
+  })
