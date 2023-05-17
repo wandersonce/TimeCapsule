40bdcca4a3c1c00374c60d8e9ec54b72eac838a6
Wanderson Castro <wandersonce1@hotmail.com>
2023-05-17 11:22:23 -0700

    Adding Copyright and Hero

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,8 @@
 import { User } from 'lucide-react'
+import Image from 'next/image'
+
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+
 export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
@@ -22,6 +26,21 @@ export default function Home() {
         </a>
 
         {/* Hero*/}
+        <div className='space-y-5'>
+          <Image src={nlwLogo} alt="Nlw Time Capsule" />
+
+          <div className='max-w-[420px] space-y-4'>
+            <h1 className=' text-5xl font-bold leading-tight text-gray-50'>Your Time Capsule</h1>
+            <p className='mt-1 text-lg leading-relaxed'>Collect memorable moments from your journey and share with the world!</p>
+          </div>
+
+          <a className='inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600' href="">REGISTER MEMORIES</a>
+        </div>
+
+        {/* Copyright*/}
+        <div className='text-sm leading-relaxed text-gray-200'>
+          Made with ❤️ with NextJs
+        </div>
       </div>
 
       {/* Right Column */}
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -57,6 +57,9 @@ module.exports = {
       backgroundSize: {
         stripes: "100% 8px",
       },
+      fontSize: {
+        "5xl": "2.5rem",
+      },
       blur: {
         full: "194px",
       },
