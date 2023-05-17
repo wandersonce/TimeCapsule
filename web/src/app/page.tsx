19c910eb437198bbaba556f2bf3fe5cce00a1a59
Wanderson Castro <wandersonce1@hotmail.com>
2023-05-17 10:27:38 -0700

    Creating columns and blur effect

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,24 @@
 
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950">
-      <h1 className="text-zinc-50 text-4xl font-bold p-6">Your Time Capsule</h1>
-    </div>
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* Left Column */}
+      <div className="relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden">
+        {/* Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+      
+        {/* Stripes*/}
+        <div className="absolute right-1 top-0 w-2 bg-stripes" />
+      </div>
+
+      {/* Right Column */}
+      <div className="flex flex-col p-16 ">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[300px]">You haven't registered any memories,
+            <a className="underline hover:text-gray-50" href=""> start now!</a> 
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
