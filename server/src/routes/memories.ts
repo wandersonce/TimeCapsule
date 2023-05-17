1622061498df6a4676fbb337abe37afadb9ccc0b
Wanderson Castro <wandersonce1@hotmail.com>
2023-05-17 13:11:01 -0700

    Creating memory get request

---
+++ server/src/routes/memories.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        except: memory.content.substring(0, 115).concat('...'),
+      }
+    })
+  })
+
+  app.get('/memories/:id', async () => {})
+
+  app.post('/memories', async () => {})
+
+  app.put('/memories/:id', async () => {})
+
+  app.delete('/memories/:id', async () => {})
+}
