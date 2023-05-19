8a567b22b69225d925378ebf05a9ed476611ed9f
Wanderson Castro <wandersonce1@hotmail.com>
2023-05-19 14:46:01 -0700

    Adding dayjs and formating date

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import EmptyMemories from "@/components/EmptyMemories";
 import { api } from "@/lib/api";
+import dayjs from "dayjs";
 import { cookies } from "next/headers";
 
 interface Memory {
@@ -26,7 +27,6 @@ export default async function Home() {
   })
 
   const memories: Memory[] = response.data
-  console.log(memories)
 
   if(memories.length == 0){
     return (
@@ -40,7 +40,7 @@ export default async function Home() {
         return(
           <div key={memory.id} className="space-y-4">
             <time className="flex items-center gap-2 text-sm text-gray-100 -ml-8 before:h-px before:w-5 before:bg-gray-50">
-              {memory.createdAt}
+              {dayjs(memory.createdAt).format('D[ of ]MMMM[, ]YYYY')}
             </time>
           </div>
         )
